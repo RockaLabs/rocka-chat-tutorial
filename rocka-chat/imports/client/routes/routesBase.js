263108dd03d72e51546efdfe267d0854b621fd05
Sergio Alexander <xergioalex@gmail.com>
2016-09-27 15:42:02 -0500

    Step 06: User login

--- rocka-chat/imports/client/routes/routesBase.js
+++ rocka-chat/imports/client/routes/routesBase.js
@@ -1,5 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Accounts } from 'meteor/accounts-base';
+import { Tracker } from 'meteor/tracker';
+
+Accounts.onLogin(function () {
+	FlowRouter.go('app.chat');
+});
+
+Tracker.autorun(function () {
+	// Redirect to login when user logout
+	if (!Meteor.userId()) {
+		var routeName = FlowRouter.getRouteName();
+		if (routeName && routeName.indexOf('app') !== -1) {
+			FlowRouter.go('account.login');
+		}
+	}
+});
 
 FlowRouter.notFound = {
     action: function() {
--- rocka-chat/imports/client/templates/account/login/login.js
+++ rocka-chat/imports/client/templates/account/login/login.js
@@ -1,2 +1,58 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { ValidationsAccountForm } from '/imports/client/services/validationsAccountForm.js';
+
+
 // ----- Template -----
 import './login.html';
+
+// ----- Events -----
+
+Template.login.onCreated(function () {
+	// Form vars
+	this.formValues = new ReactiveDict();
+	this.formErrors = new ReactiveDict();
+	this.formIsValid = new ReactiveVar();
+});
+
+
+Template.login.onRendered(function () {
+
+});
+
+Template.login.events({
+	'submit #loginForm'(event, instance) {
+		event.preventDefault();
+		instance.formValues.set('username', instance.$('#username').val().toLowerCase());
+		instance.formValues.set('password', instance.$('#password').val());
+
+		if (ValidationsAccountForm.validate(instance)) {
+			Meteor.loginWithPassword(instance.formValues.get('username'), instance.formValues.get('password'), (err) => {
+				if (err) {
+					console.log(err.reason);
+					instance.formErrors.set('form', err.reason);
+				} else {
+					console.log('User logged');
+				}
+			})
+		} else {
+			console.log('Error');
+		}
+	}
+});
+
+
+// ----- Helpers -----
+
+Template.login.helpers({
+	errors() {
+		const instance = Template.instance();
+		return {
+			username: instance.formErrors.get('username'),
+			password: instance.formErrors.get('password'),
+			form: instance.formErrors.get('form'),
+		};
+	},
+});
